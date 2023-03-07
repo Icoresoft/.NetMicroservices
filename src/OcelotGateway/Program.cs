c962e616a787293d8cc3251a0dc9e52693c87906
Icoresoft <coresoft.eg@gmail.com>
2023-03-07 10:28:18 +0300

    Start Ocelot Gateway

--- src/OcelotGateway/Program.cs
+++ src/OcelotGateway/Program.cs
@@ -1,7 +1,24 @@
+using Ocelot.Middleware;
+/*Ocelot gateway 
+ * Free - Open Source API gateway for .net platform 
+ * 
+ * 
+ */
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.ConfigureLogging(logging =>
+{
+    logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+    logging.ClearProviders();
+    logging.AddConsole();
+    logging.AddDebug();
+});
+
+//builder.Con
 var app = builder.Build();
 
+
+app.UseOcelot();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
